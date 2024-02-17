3a3fd1c7db61aa1cfc32f600ebf3cc247181a310
MikheevStanislav <145530527+MikheevStanislav@users.noreply.github.com>
2024-02-17 15:13:01 +0700

    4

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ class NameSearchTree:
             return [node]
         else:
             leaf_nodes = []
-            leaf_nodes.extend(self._find_leaf(node.left))
-            leaf_nodes.extend(self._find_leaf(node.right))
+            for ch in node.child:
+                leaf_nodes.extend(self._find_leaf(node.child[ch]))
             return leaf_nodes
 
     # Возвращает все подходящие под описание объекты
@@ -55,8 +55,8 @@ class NameSearchTree:
                 return None
             current = current.child[ch]
             if i == len(name) - 1:
-                if(current.child is not None):
-                    return(self._find_leaf(current))
+                if (current.child is not None):
+                    return (self._find_leaf(current))
                 else:
                     return [current]
 
@@ -92,6 +92,7 @@ class Person:
 
 class Table:
     def __init__(self, pathfile):
+        self.organisations_tree = None
         self.pathfile = pathfile
         self.persons_array = []
         self.names_tree = NameSearchTree()
@@ -101,7 +102,8 @@ class Table:
         self.personal_number_to_person = {}
         self.work_number_to_person = {}
         with open(self.pathfile + '.txt', 'r', encoding='utf-8') as file:
-            for i, line in enumerate(file):
+            i = 0
+            for line in file:
                 if line == "\n":
                     continue
                 name, family_name, fathers_name, organisation_name, work_number, personal_number = line.split(',')
@@ -110,8 +112,9 @@ class Table:
                                     self.organisation_names_tree.insert(organisation_name, i),
                                     work_number, personal_number, i)
                 self.persons_array.append(new_person)
-                self.personal_number_to_person[personal_number[:-1]] = self.persons_array[-1]
+                self.personal_number_to_person[personal_number.strip()] = self.persons_array[-1]
                 self.work_number_to_person[work_number] = self.persons_array[-1]
+                i += 1
 
     def insert_data(self, name: str, family_name: str, fathers_name: str, organisation_name: str, work_number: str,
                     personal_number: str) -> Person:
@@ -120,7 +123,10 @@ class Table:
                             self.fathers_names_tree.insert(fathers_name, i),
                             self.organisation_names_tree.insert(organisation_name, i),
                             work_number, personal_number, i)
-        if self._search_father_name(fathers_name, self._search_family(family_name, self._search_name(name))) is not None:
+        if self.search(name, family_name, fathers_name, organisation_name) is not None and len(self.search(name,
+                                                                                                           family_name,
+                                                                                                           fathers_name,
+                                                                                                           organisation_name)) != 0:
             print("Этот объект уже есть")
             return None
         self.persons_array.append(new_person)
@@ -133,7 +139,7 @@ class Table:
             file.close()
         return new_person
 
-    def _read_data(self, index: int) -> None:
+    def read_data(self, index: int) -> None:
         print(str(index) + ":" + self.persons_array[index].family_name.name + " " + self.persons_array[
             index].name.name + " " +
               self.persons_array[index].fathers_name.name + ", " + self.persons_array[
@@ -143,23 +149,26 @@ class Table:
 
     def read_all_data(self) -> None:
         for i in range(len(self.persons_array)):
-            self._read_data(i)
+            self.read_data(i)
 
-    def _search_number(self, string: str) -> Person:
+    def search_number(self, string: str) -> Person:
         if string in self.personal_number_to_person:
             return self.personal_number_to_person[string]
         if string in self.work_number_to_person:
             return self.work_number_to_person[string]
         else:
             print("Такого номера не найдено в базе данных")
+            return None
 
     def _search_name(self, string: str, mas=None) -> []:
         ans = []
+        if string is None:
+            return mas
         if self.names_tree.search(string) is None:
             return
         for x in self.names_tree.search(string):
             for i in x.indexes:
-                if mas == None:
+                if mas is None:
                     ans.append(i)
                 else:
                     if i in mas:
@@ -168,11 +177,13 @@ class Table:
 
     def _search_family(self, string, mas=None) -> []:
         ans = []
+        if string is None:
+            return mas
         if self.family_names_tree.search(string) is None:
             return
         for x in self.family_names_tree.search(string):
             for i in x.indexes:
-                if mas == None:
+                if mas is None:
                     ans.append(i)
                 else:
                     if i in mas:
@@ -181,11 +192,13 @@ class Table:
 
     def _search_father_name(self, string, mas=None) -> []:
         ans = []
+        if string is None:
+            return mas
         if self.fathers_names_tree.search(string) is None:
             return
         for x in self.fathers_names_tree.search(string):
             for i in x.indexes:
-                if mas == None:
+                if mas is None:
                     ans.append(i)
                 else:
                     if i in mas:
@@ -194,15 +207,25 @@ class Table:
 
     def _search_organisation_name(self, string, mas=None) -> []:
         ans = []
-        if self.organisations_tree.search(string) is None:
+        if string is None:
+            return mas
+        if self.organisation_names_tree.search(string) is None:
             return
         for x in self.organisation_names_tree.search(string):
-            if mas == None:
+            if mas is None:
                 ans.append(x)
             else:
                 if x in mas:
                     ans.append(x)
         return ans
+
+    def search(self, name=None, family_name=None, father_name=None, organisation_name=None) -> []:
+        if (name is None and family_name is None and father_name is None and organisation_name is None):
+            return (None)
+        return self._search_organisation_name(organisation_name, self._search_father_name(father_name,
+                                                                                          self._search_family(
+                                                                                              family_name,
+                                                                                              self._search_name(name))))
 
     def change(self, index: int):
         print("Введите имя(не вводите ничего если оставить неизмененным)")
@@ -227,30 +250,44 @@ class Table:
         string = input()
         if string:
             temp = self.persons_array[index].organisation_name
-            if(temp.name == string):
+            if (temp.name == string):
                 print("Имя тоже что и было")
             else:
                 self.persons_array[index].organisation_name = self.organisation_names_tree.insert(string, index)
                 self.organisation_names_tree.delete(temp)
         print("Введите рабочий номер телефона(не вводите ничего если оставить неизмененным)")
-        string = input()
-        if string:
-            self.work_number_to_person[string] = self.persons_array[index]
+        string1 = input()
+        if string1:
+            if string1 in self.personal_number_to_person or string1 in self.work_number_to_person:
+                print("Этот номер занят")
+                return
+            self.work_number_to_person[string1] = self.persons_array[index]
             del self.work_number_to_person[self.persons_array[index].work_number]
-            self.persons_array[index].work_number = string
+            self.persons_array[index].work_number = string1
 
         print("Введите домашний номер телефона(не вводите ничего если оставить неизмененным)")
         string = input()
         if string:
-            self.personal_number_to_person[string] = self.persons_array[index]
-            del self.personal_number_to_person[self.persons_array[index].personal_number]
-            self.persons_array[index].personal_number = string
+            if string1 == string:
+                self.personal_number_to_person[string] = self.persons_array[index]
+                del self.personal_number_to_person[self.persons_array[index].personal_number.strip()]
+                self.persons_array[index].personal_number = string
+            else:
+                if string in self.personal_number_to_person or string in self.work_number_to_person:
+                    print("Этот номер занят")
+                    return
+                self.personal_number_to_person[string] = self.persons_array[index]
+                del self.personal_number_to_person[self.persons_array[index].personal_number]
+                self.persons_array[index].personal_number = string
+        print("Данные успешно обновлены")
+        self._save_changes()
 
     def _save_changes(self):
         with open(self.pathfile + '.txt', 'w', encoding='utf-8') as file:
             for i in range(len(self.persons_array)):
                 file.write(
-                    self.persons_array[i].name.name + ", " + self.persons_array[i].family_name.name + ", " + self.persons_array[
+                    self.persons_array[i].name.name + "," + self.persons_array[i].family_name.name + "," +
+                    self.persons_array[
                         i].fathers_name.name +
                     "," + self.persons_array[i].organisation_name.name + "," + self.persons_array[i].work_number + "," +
                     self.persons_array[i].personal_number + "\n")
@@ -258,39 +295,115 @@ class Table:
 
 print("Добро пожаловать в телефонный справочник")
 table1 = Table("base")
+arr = table1._search_name("Артем")
 while (True):
-
     print(
         "Выберите действие(введите номер в терминал):" + '\n' + "1. Вывести весь справочник" + '\n' +
         "2. Вставить новый объект" + '\n' + "3. Найти запись в справочнике" + '\n' + "4. Редактировать запись" +
-        '\n' + "5. Удалить номер" + '\n' + "6. Выход")
-    choice = input()
-    if (choice == '6'):
+        '\n' + "5. Выход")
+    choice = str(input())
+    if choice == "":
+        print("Неопознанная команда")
+        continue
+    if choice[0] == '5':
         print("До новых встреч!")
         table1._save_changes()
         break
-    if (choice == '1'):
+    if choice[0] == '1':
         table1.read_all_data()
-    if (choice == '2'):
+    if choice[0] == '2':
         mas = []
-        print("Введите фамилию владельца номера")
+        print("Введите фамилию владельца номера(нажмите ENTER чтобы пропустить)")
         mas.append(input())
-        print("Введите имя владельца номера")
+        print("Введите имя владельца номера(нажмите ENTER чтобы пропустить)")
         mas.append(input())
         print("Введите отчество")
         mas.append(input())
-        print("Введите название организации")
+        print("Введите название организации(нажмите ENTER чтобы пропустить)")
         mas.append(input())
-        print("Введите рабочий номер")
+        print("Введите рабочий номер(нажмите ENTER чтобы пропустить)")
         mas.append(input())
-        print("Введите домашний номер")
+        print("Введите домашний номер(нажмите ENTER чтобы пропустить)")
         mas.append(input())
-        for i in range(mas):
-            if(mas[i] == ""):
+        if (mas[4] in table1.work_number_to_person or mas[5] in table1.work_number_to_person
+                or mas[4] in table1.personal_number_to_person or mas[5] in table1.personal_number_to_person):
+            print("Человек с таким номером уже существует")
+            continue
+        for i in range(len(mas)):
+            if mas[i] == "":
                 mas[i] = "_"
         table1.insert_data(mas[1], mas[0], mas[2], mas[3], mas[4], mas[5])
-    if (choice == "3"):
-
-
-
-
+    if choice[0] == "3":
+        print(
+            "Выберите вариант поиска:" + "\n" + "1: По номеру телефона(любого)" + "\n" + "2: По имени/фамилии/отчеству/имени организации")
+        choice2 = input()
+        if choice2 == '1':
+            print("Введите номер:")
+            person = table1.search_number(input())
+            if person == None:
+                continue
+            table1.read_data(person.index)
+        if choice2 == '2':
+            mas = []
+            print("Введите фамилию владельца номера(нажмите ENTER чтобы пропустить)")
+            mas.append(input())
+            print("Введите имя владельца номера (нажмите ENTER чтобы пропустить)")
+            mas.append(input())
+            print("Введите отчество (нажмите ENTER чтобы пропустить)")
+            mas.append(input())
+            print("Введите название организации (нажмите ENTER чтобы пропустить)")
+            mas.append(input())
+            for i in range(0, len(mas)):
+                if mas[i] == "":
+                    mas[i] = None
+            temp = table1.search(mas[1], mas[0], mas[2], mas[3])
+            if temp is None or len(temp) == 0:
+                print("Не найдено таких записей")
+                continue
+            for x in temp:
+                table1.read_data(x)
+        else:
+            print("Неопознанная команда")
+            continue
+    if choice[0] == "4":
+        print(
+            "Выберите вариант поиска:" + "\n" + "1: По номеру телефона(любого)" + "\n" + "2: По имени/фамилии/отчеству/имени организации")
+        choice2 = input()
+        if choice2 == '1':
+            print("Введите номер:")
+            person = table1.search_number(input())
+            table1.read_data(person.index)
+            table1.change(person.index)
+        if choice2 == '2':
+            mas = []
+            print("Введите фамилию владельца номера(нажмите ENTER чтобы пропустить)")
+            mas.append(input())
+            print("Введите имя владельца номера (нажмите ENTER чтобы пропустить)")
+            mas.append(input())
+            print("Введите отчество (нажмите ENTER чтобы пропустить)")
+            mas.append(input())
+            print("Введите название организации (нажмите ENTER чтобы пропустить)")
+            mas.append(input())
+            for i in range(0, len(mas)):
+                if mas[i] == "":
+                    mas[i] = None
+            temp = table1.search(mas[1], mas[0], mas[2], mas[3])
+            if temp is None or len(temp) == 0:
+                print("Не найдено таких записей")
+                continue
+            for i,x in enumerate(temp):
+                print(str(i) + ": ")
+                table1.read_data(x)
+            print("Выберите запись")
+            choice3 = input()
+            if not choice3.isdigit() or int(choice3) > len(temp) or int(choice3) < 0:
+                print("Неопознанная команда")
+                continue
+            else:
+                table1.change(temp[int(choice3)])
+        else:
+            print("Неопознанная команда")
+            continue
+    if choice[0] != 1 and choice[0] != 2 and choice[0] != 3 and choice[0] !=4:
+        print("Неопознанная команда")
+        continue
