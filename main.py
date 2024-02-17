3071bbc39e44b4493011d1f7b4b182a794d1a208
MikheevStanislav <145530527+MikheevStanislav@users.noreply.github.com>
2024-02-17 16:25:15 +0700

    final

--- main.py
+++ main.py
@@ -1,41 +1,62 @@
 from typing import Any
 import os
 
-
 # Для начала создадим класс, который хранит из имен номера. Это будет Древо поиска(префиксное дерево), т.к пользователь может не знать
 # имени искомого человека целиком. Если мы реализуем хранение пары имя -> номер то временная сложность при удобном
 # для пользователя способе(к примеру есть имена Артем, Анатолий, Борис, пользователь воодит А, программа выдает
 # Артема и Анатолия) ьудет O(N), а у дерева O(logN) P.S именно ради этой фичи нужен этот класс. Т.к. для пары телефон
 # -> такая фича не нужна, мы можем использовать простую хэштаблицу.
+
 class SearchTreeNode:
+    """
+        Конструктор для создания узла дерева поиска.
+
+        :param name: Имя конкретной ноды (строка).
+        :param parent: Родительская нода (SearchTreeNode).
+        :param person: Объект класса person, к которому принадлежит эта нода.
+    """
     def __init__(self, name=None, parent=None, person=None):
         self.name = name  # Имя конкретной ноды
         self.parent = parent #отец ноды
         self.child = {}  # Хэштаблица всех сыновей ноды
         self.person = person #объект класса person к которой эта нода принадлежит
-        self.indexes = set()
+        self.indexes = set() #индексы person с таким именем\фамилией\отчеством\именем организации
 
 
 class NameSearchTree:
     def __init__(self):
+        """
+                Конструктор для создания древа поиска имен.
+        """
         self.root = SearchTreeNode("")  # создаем корень. Корень не будет иметь имени
 
     # метод добавления данных в древо
+
     def insert(self, name, index) -> SearchTreeNode:
-        current = self.root  # заводим переменную текущей ноды
-        for i in range(
-                len(name)):  # проходимся по всему имени. Для каждой буквы делаем ноду, имя которой являются все
-            # буквы то текущей буквы. Если буква последняя, даем ноде индекс
+        """
+        Метод для добавления данных в древо.
+
+        :param name: Имя для добавления в древо (строка).
+        :param index: Индекс связанный с именем (число).
+        :return: Возвращает новый узел древа (SearchTreeNode).
+        """
+        current = self.root  # Начинаем с корня
+        for i in range(len(name)):  # Проходим по каждой букве в имени
             ch = name[i]
-            if ch not in current.child:
+            if ch not in current.child:  # Если такой буквы нет, создаем новую ноду
                 current.child[ch] = SearchTreeNode(str(current.name) + str(ch), current)
             current = current.child[ch]
-            if i == len(name) - 1:
+            if i == len(name) - 1:  # Если это последняя буква, добавляем индекс
                 current.indexes.add(index)
         return current
 
-    # Вспомогательный метод, для поиска всех листов конкретной вершины. Нужен для поиска по древу.
     def _find_leaf(self, node) -> []:
+        """
+        Вспомогательный метод для поиска всех листьев конкретной вершины. Используется для поиска по древу.
+
+        :param node: Нода, с которой начинается поиск (SearchTreeNode).
+        :return: Список листьев (list).
+        """
         if node is None:
             return []
         if len(node.child) == 0:
@@ -46,8 +67,12 @@ class NameSearchTree:
                 leaf_nodes.extend(self._find_leaf(node.child[ch]))
             return leaf_nodes
 
-    # Возвращает все подходящие под описание объекты
     def search(self, name: str) -> []:
+        """
+        Возвращает все подходящие под описание объекты
+        :param name: имя искомой строки в префиксном древе
+        :return: массив строк префикс которых совпадает с name
+        """
         current = self.root
         for i in range(len(name)):
             ch = name[i]
@@ -60,8 +85,12 @@ class NameSearchTree:
                 else:
                     return [current]
 
-    #метод удаления ноды. Если нода не имеет братьев, ее отца также нужно удалить
     def delete(self, node: SearchTreeNode) -> bool:
+        """
+        метод удаления ноды. Если нода не имеет братьев, ее отца также нужно удалить
+        :param node: узел который нужно удалить
+        :return:
+        """
         name = node.name
         ans = self.search(name)
         if ans is None or len(ans) == 0:
@@ -78,10 +107,23 @@ class NameSearchTree:
             index = int(input()) - 1
             del ans[index].parent.child[ans[index].name[-1]]
 
-#класс человека, который вводит биекцию между комбинацией ФИО и 2 номерами телефона
+
 class Person:
+    """
+    класс человека, который вводит биекцию между комбинацией ФИО и 2 номерами телефона
+    """
     def __init__(self, name: SearchTreeNode, family_name: SearchTreeNode, fathers_name: SearchTreeNode,
                  organisation_name: SearchTreeNode, work_number: str, personal_number: str, index: int):
+        """
+        инициализация
+        :param name: имя человека
+        :param family_name: фамилия человека
+        :param fathers_name: отчество
+        :param organisation_name: название организации
+        :param work_number: рабочий телефон
+        :param personal_number: личный телефон
+        :param index: индекс в массиве внутри table
+        """
         self.name = name
         self.family_name = family_name
         self.fathers_name = fathers_name
@@ -90,20 +132,26 @@ class Person:
         self.personal_number = personal_number
         self.index = index
 
-#класс таблицы. Хранит путь до объекта, массив людей, а также хэшмапы из номеров телефонов в людей
+
 class Table:
+    """
+    класс таблицы. Хранит путь до объекта, массив людей, а также хэшмапы из номеров телефонов в людей
+    """
     #при инициализации открывает путь до файла и считывает всю информацию из него в префиксные деервья и хэшмапы(для имени, фамилии, отчества свои префиксные деревья)
     def __init__(self, pathfile):
-        self.organisations_tree = None
+        """
+        инициализация таблицы
+        :param pathfile: путь до файла с базой
+        """
         self.pathfile = pathfile
-        self.persons_array = []
-        self.names_tree = NameSearchTree()
-        self.family_names_tree = NameSearchTree()
-        self.fathers_names_tree = NameSearchTree()
-        self.organisation_names_tree = NameSearchTree()
-        self.personal_number_to_person = {}
-        self.work_number_to_person = {}
-        with open(self.pathfile + '.txt', 'r', encoding='utf-8') as file:
+        self.persons_array = [] #массив объектов person
+        self.names_tree = NameSearchTree() #префиксное древо для имени
+        self.family_names_tree = NameSearchTree()#для фамилии
+        self.fathers_names_tree = NameSearchTree()#для отчества
+        self.organisation_names_tree = NameSearchTree()#для названия организации
+        self.personal_number_to_person = {}#хэшмапа для личного номера
+        self.work_number_to_person = {}#для рабочего
+        with open(self.pathfile + '.txt', 'r', encoding='utf-8') as file: # загрузка информации из файла
             i = 0
             for line in file:
                 if line == "\n":
@@ -118,9 +166,19 @@ class Table:
                 self.work_number_to_person[work_number] = self.persons_array[-1]
                 i += 1
 
-    #функция вставки данных
+
     def insert_data(self, name: str, family_name: str, fathers_name: str, organisation_name: str, work_number: str,
                     personal_number: str) -> Person:
+        """
+        функция вставки данных
+        :param name: имя
+        :param family_name: фамилия
+        :param fathers_name: отчество
+        :param organisation_name: название организации
+        :param work_number: номер телефона для работы
+        :param personal_number: личный номер теелфона
+        :return:
+        """
         i = len(self.persons_array)
         new_person = Person(self.names_tree.insert(name, i), self.family_names_tree.insert(family_name, i),
                             self.fathers_names_tree.insert(fathers_name, i),
@@ -142,8 +200,13 @@ class Table:
             file.close()
         return new_person
 
-    #функция прочтения данных из person в массиве persons_array
+
     def read_data(self, index: int) -> None:
+        """
+        функция прочтения данных из person в массиве persons_array
+        :param index: индекс person(который выводим на экран) в массиве person_array
+        :return:
+        """
         print(str(index) + ":" + self.persons_array[index].family_name.name + " " + self.persons_array[
             index].name.name + " " +
               self.persons_array[index].fathers_name.name + ", " + self.persons_array[
@@ -151,13 +214,22 @@ class Table:
               self.persons_array[index].work_number + "      Домашний номер:" + self.persons_array[
                   index].personal_number)
 
-    #использование предыдущего метода на все элементы массива persons_array
+
     def read_all_data(self) -> None:
+        """
+        использование предыдущего метода на все элементы массива persons_array
+        :return:
+        """
         for i in range(len(self.persons_array)):
             self.read_data(i)
 
-    #поиск person по хэшапам номеров
+
     def search_number(self, string: str) -> Person:
+        """
+        поиск person по хэшапам номеров
+        :param string: номер телефона
+        :return:
+        """
         if string in self.personal_number_to_person:
             return self.personal_number_to_person[string]
         if string in self.work_number_to_person:
@@ -168,6 +240,12 @@ class Table:
 
     #поиск по префиксным деревьям
     def _search_name(self, string: str, mas=None) -> []:
+        """
+        поиск по префиксному дереву имен
+        :param string: имя
+        :param mas: в случае если этот поиск не первый(то есть мы отобрали всех с некотрой фамилией\отчеством\названием организации и хотим из них отлбрать всех с некотрым префиксом имени)
+        :return:
+        """
         ans = []
         if string is None:
             return mas
@@ -183,6 +261,12 @@ class Table:
         return ans
 
     def _search_family(self, string, mas=None) -> []:
+        """
+        поиск по префиксному дереву фамилий
+        :param string: фамилия
+        :param mas:  случае если этот поиск не первый(то есть мы отобрали всех с некотрой именем\отчеством\названием организации и хотим из них отлбрать всех с некотрым префиксом фамилии
+        :return:
+        """
         ans = []
         if string is None:
             return mas
@@ -198,6 +282,12 @@ class Table:
         return ans
 
     def _search_father_name(self, string, mas=None) -> []:
+        """
+        аналогично по отчеству
+        :param string:
+        :param mas:
+        :return:
+        """
         ans = []
         if string is None:
             return mas
@@ -213,6 +303,12 @@ class Table:
         return ans
 
     def _search_organisation_name(self, string, mas=None) -> []:
+        """
+        и по имени организации
+        :param string:
+        :param mas:
+        :return:
+        """
         ans = []
         if string is None:
             return mas
@@ -227,6 +323,14 @@ class Table:
         return ans
 
     def search(self, name=None, family_name=None, father_name=None, organisation_name=None) -> []:
+        """
+        суммарный поиск по всем 4 параметрам
+        :param name:
+        :param family_name:
+        :param father_name:
+        :param organisation_name:
+        :return:
+        """
         if (name is None and family_name is None and father_name is None and organisation_name is None):
             return (None)
         return self._search_organisation_name(organisation_name, self._search_father_name(father_name,
@@ -235,6 +339,11 @@ class Table:
                                                                                               self._search_name(name))))
 
     def change(self, index: int):
+        """
+        функция изменения некотрого элемента
+        :param index:
+        :return:
+        """
         print("Введите имя(не вводите ничего если оставить неизмененным)")
         string = input()
         if string:
